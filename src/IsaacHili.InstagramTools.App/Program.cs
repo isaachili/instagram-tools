9083cb176213dfbcd813fc4e881f5212304409c1
Isaac Hili <18428880+isaachili@users.noreply.github.com>
2024-10-26 17:00:08 +0200

    Configure Blazor WASM project

---
+++ src/IsaacHili.InstagramTools.App/Program.cs
@@ -0,0 +1,16 @@
+using IsaacHili.InstagramTools.App;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+_ = builder.Services.AddScoped(sp => 
+	new HttpClient 
+	{
+		BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+	});
+
+var host = builder.Build();
+await host.RunAsync();
