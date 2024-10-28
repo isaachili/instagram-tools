9bd1ece300fd2f0dd3c52ad872bcfc3ec8707dc2
Isaac Hili <18428880+isaachili@users.noreply.github.com>
2024-10-28 18:54:12 +0100

    Document ConnectionTypes and FollowersExtensions

--- src/IsaacHili.InstagramTools.Followers.Abstractions/Enums/ConnectionTypes.cs
+++ src/IsaacHili.InstagramTools.Followers.Abstractions/Enums/ConnectionTypes.cs
@@ -1,10 +1,29 @@
 namespace IsaacHili.InstagramTools.Followers.Abstractions.Enums;
 
+/// <summary>
+/// Connection type
+/// </summary>
 [Flags]
 public enum ConnectionTypes
 {
+	/// <summary>
+	/// No connection
+	/// </summary>
 	None = 0,
+	
+	/// <summary>
+	/// Subject is following the connection.
+	/// </summary>
 	Following = 1,
+	
+	/// <summary>
+	/// Subject is followed by the connecion.
+	/// </summary>
 	Follower = 2,
+	
+	/// <summary>
+	/// Subject is followed by the connection,
+	/// and is also following the connection.
+	/// </summary>
 	Mutual = Following | Follower,
 }
--- src/IsaacHili.InstagramTools.Followers/Extensions/FollowersExtensions.cs
+++ src/IsaacHili.InstagramTools.Followers/Extensions/FollowersExtensions.cs
@@ -6,6 +6,13 @@ namespace IsaacHili.InstagramTools.Followers.Extensions;
 
 public static class FollowersExtensions
 {
+	/// <summary>
+	/// Adds the services for the followers/connections module.
+	/// </summary>
+	/// <param name="services">Service collection</param>
+	/// <returns>
+	/// The <paramref name="services"/> service collection instance.
+	/// </returns>
 	public static IServiceCollection AddFollowers(this IServiceCollection services)
 	{
 		// Inject ConnectionsService
