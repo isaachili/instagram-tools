bc1fa4ba0f221115e0848f6b4abc18376ccf62c1
Isaac Hili <18428880+isaachili@users.noreply.github.com>
2024-11-03 21:14:56 +0000

    Style Instagram connections tool (#2)
    * Create NPM `packages.json`
    * Add support for Tailwind CSS
    * Style Instagram connections tool
    * Make website responsive
    * Add website headers and footers

---
+++ src/IsaacHili.InstagramTools.App/wwwroot/tailwind.config.js
@@ -0,0 +1,28 @@
+import colors from 'tailwindcss/colors';
+
+export const content = ['../**/*.{html,razor}'];
+export const theme = {
+	colors: {
+		transparent: 'transparent',
+		sky: colors.sky,
+		slate: colors.slate,
+		red: {
+			400: colors.red[400],
+		},
+		orange: {
+			300: colors.orange[300],
+		},
+		mustard: '#ffc84c',
+		midnight: '#0e1825',
+	},
+	fontFamily: {
+		'mono': [ 'JetBrains Mono' ],
+		'sans': [ 'Raleway' ]
+	},
+	extend: {
+		gridTemplateColumns: {
+			'1fr-3fr': '1fr 3fr',
+		},
+	}
+};
+export const plugins = [];
